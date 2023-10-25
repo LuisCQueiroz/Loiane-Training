3135aea760ec8135a7421d1b0b8e61ed4b629a90
LuisCQueiroz <djlc2@yahoo.com.br>
2023-10-25 02:08:31 -0300

    Inicio Exercício 17

--- Java Básico/CursoJava/src/com/luis/cursojava/exercicios01a13/Exercicio17.java
+++ Java Básico/CursoJava/src/com/luis/cursojava/exercicios01a13/Exercicio17.java
@@ -1,11 +1,43 @@
 package com.luis.cursojava.exercicios01a13;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Exercicio17 {
 
 	public static void main(String[] args) {
- /*
-  * Teste de Git
-  */
+		
+		Scanner scan = new Scanner(System.in);
+		
+		Locale localeBR = new Locale("pt", "BR");
+		
+		NumberFormat inteiro = NumberFormat.getInstance();
+		NumberFormat dinheiro = NumberFormat.getCurrencyInstance();
+		NumberFormat numberFormat = NumberFormat.getInstance(localeBR);
+
+		double cobertura = 6;
+		double lataCobertura = 18 * cobertura;
+		double precoLata = 80;
+		double galao = 3.6 * cobertura;
+		double precoGalao = 25;
+		
+		System.out.println("Informe o tamanho em metros quadrados da àrea a ser pintada:");
+		double area = scan.nextDouble();
+		
+		double areaPintar = (area + ((area * 10)/100));
+		
+		double areacoberta = Math.round(areaPintar / lataCobertura);
+		
+		if (areacoberta < 1) {
+			areacoberta=1;
+		}
+		
+		double precoCobertura = areacoberta * precoLata;
+		
+		System.out.println("Uma lata de 18 litros cobre: " + lataCobertura + " metros.");
+		System.out.println("Você irá utilizar " + areacoberta + " latas de 18 litros. O preço para compra é " + dinheiro.format(precoCobertura));
+		
 
 	}
 
